f9c5f70e34771c244e473ddaad3730bdb3ffc01c
BetterPlay <betterplaycompany@gmail.com>
2023-01-30 19:09:17 +0100

    Initial Commit

---
+++ BetterPlay/BetterPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterPlayApp.swift
+//  BetterPlay
+//
+//  Created by Apps2T on 30/1/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
