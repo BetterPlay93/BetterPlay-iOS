b12b1984bd4ab27853e8cc8462e6e6cacc48e653
BetterPlay <betterplaycompany@gmail.com>
2023-02-09 16:59:04 +0100

    BET-23
    Esqueleto

---
+++ BetterPlay/Presentation/Register/Model/AuthenticationPresentationModel.swift
@@ -0,0 +1,12 @@
+//
+//  AuthenticationPresentationModel.swift
+//  BetterPlay
+//
+//  Created by Apps2T on 9/2/23.
+//
+
+import Foundation
+
+class AuthenticationResponseModel {
+    
+}
---
+++ BetterPlay/Presentation/Register/RegisterView-ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterView-ViewModel.swift
+//  BetterPlay
+//
+//  Created by Apps2T on 9/2/23.
+//
+
+import Foundation
+
+extension RegisterView {
+    class ViewModel: ObservableObject {
+        
+    }
+}
