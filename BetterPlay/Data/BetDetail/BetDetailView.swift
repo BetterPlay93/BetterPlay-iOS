7539541d0765c679ff11180fd95e66251159803f
BetterPlay <betterplaycompany@gmail.com>
2023-02-09 17:54:11 +0100

    BET-148
    Esqueleto

---
+++ BetterPlay/Data/BetDetail/BetDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  BetDetailView.swift
+//  BetterPlay
+//
+//  Created by Apps2T on 9/2/23.
+//
+
+import SwiftUI
+
+struct BetDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BetDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        BetDetailView()
+    }
+}
