fbd2b727a1f3e952e411d34255a985ff4395cf48
BetterPlay <betterplaycompany@gmail.com>
2023-02-09 17:27:04 +0100

    BET-76
    Esqueleto

---
+++ BetterPlay/Presentation/EditProfile/EditProfileView-ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  EditProfileView-ViewModel.swift
+//  BetterPlay
+//
+//  Created by Apps2T on 9/2/23.
+//
+
+import Foundation
+
+extension EditProfileView {
+    class ViewModel: ObservableObject {
+        
+    }
+}
---
+++ BetterPlay/Presentation/EditProfile/EditProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  EditProfileView.swift
+//  BetterPlay
+//
+//  Created by Apps2T on 9/2/23.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}
