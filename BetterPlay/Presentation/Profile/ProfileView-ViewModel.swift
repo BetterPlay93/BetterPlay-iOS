66efd1d5d0ae5f97e6ac1da26c5d5e2a752e568b
BetterPlay <betterplaycompany@gmail.com>
2023-02-09 17:33:08 +0100

    BET-65
    Esqueleto

---
+++ BetterPlay/Presentation/Profile/ProfileView-ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileView-ViewModel.swift
+//  BetterPlay
+//
+//  Created by Apps2T on 9/2/23.
+//
+
+import Foundation
+
+extension ProfileView {
+    class ViewModel: ObservableObject {
+        
+    }
+}
