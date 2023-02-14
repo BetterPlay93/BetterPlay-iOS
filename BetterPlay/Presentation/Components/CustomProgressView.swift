c6506169d6f5d7b9d9cd7b0bb4e0b6aff6c00bec
BetterPlay <betterplaycompany@gmail.com>
2023-02-14 19:45:41 +0100

    BET-66
    Finish Profile Views

---
+++ BetterPlay/Presentation/Components/CustomProgressView.swift
@@ -0,0 +1,29 @@
+//
+//  CustomProgressView.swift
+//  BetterPlay
+//
+//  Created by Apps2T on 13/2/23.
+//
+
+import SwiftUI
+
+struct CustomProgressView: View {
+    var progressColor: String
+    var progressBackgroundColor: String
+    var value: Int
+    
+    var body: some View {
+        ProgressView("", value: Float(value), total: 100)
+             .tint(Color(progressColor))
+             .background(Color(progressBackgroundColor))
+             .scaleEffect(x: 1, y: 5, anchor: .bottom)
+             .frame(height: 10)
+             .clipShape(RoundedRectangle(cornerRadius: 5))
+    }
+}
+
+struct CustomProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomProgressView(progressColor: "Green", progressBackgroundColor: "White", value: 30)
+    }
+}
---
+++ BetterPlay/Presentation/Components/ProfileNavBar.swift
@@ -0,0 +1,78 @@
+//
+//  ProfileNavBar.swift
+//  BetterPlay
+//
+//  Created by Apps2T on 14/2/23.
+//
+
+import SwiftUI
+
+struct ProfileNavBar: View {
+    
+    var username: String
+    @State var selection: Int = 0
+    
+    var body: some View {
+        VStack{
+            navBar
+            
+            filter
+        }.background(Color("Background2"))
+    }
+    
+    // MARK: - Accesory View
+    var navBar: some View {
+        ZStack(){
+            Rectangle()
+                .fill(Color("Gray")).frame(maxWidth: .infinity, maxHeight: 50)
+            HStack{
+                Text(username)
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 25))
+                    .bold()
+                    .padding(.leading, 20)
+                Spacer()
+                
+                Image("SearchUser")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 35, height: 35, alignment: .leading)
+                    .padding(.trailing, 10)
+                
+                Image(systemName: "gearshape.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 35, height: 35, alignment: .leading)
+                    .foregroundColor(Color.white)
+                    .padding(.trailing, 20)
+            }
+        }
+    }
+    
+    var filter: some View {
+        VStack{
+            HStack(spacing: 0){
+                Picker("", selection: $selection){
+                    Image(systemName: "person.crop.circle").tag(0)
+                    Image(systemName: "heart.fill").tag(1)
+                }.pickerStyle(SegmentedPickerStyle()).padding(20)
+                
+            }
+            
+            if selection == 0 {
+                ProfileView()
+            }else{
+                FriendProfileView()
+            }
+        }
+        
+        
+        
+    }
+}
+
+struct ProfileNavBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileNavBar(username: "Jose Ramón")
+    }
+}
