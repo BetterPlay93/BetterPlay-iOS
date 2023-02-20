24c7857754fc9d1bdbd77e1053d230e79b48bda7
BetterPlay93 <120207618+BetterPlay93@users.noreply.github.com>
2023-02-20 19:28:44 +0100

    BET-42
    Not finish

--- BetterPlay/Presentation/RecoverPassword/View/RecoverPassEmailView.swift
+++ BetterPlay/Presentation/RecoverPassword/View/RecoverPassEmailView.swift
@@ -9,51 +9,67 @@ import SwiftUI
 
 struct RecoverPassEmailView: View {
     @State private var email = ""
-            
+    @ObservedObject var viewmodel : ViewModel = ViewModel()
+    
     var body: some View {
         ZStack() {
             VStack(spacing: 10) {
-                Image("Logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 125, height: 80)
-                
+                logo
                 VStack(spacing: 2){
-                    Text("Email")
-                        .frame(maxWidth: 234, alignment: .leading)
-                    TextField("",text: $email)
-                        .padding().frame(width: 234, height: 27)
-                        .background(Color(.white))
-                        .cornerRadius(10)
-                    
-                    Button {
-                         
-                    } label: {
-                        Text("Continuar")
-                            .foregroundColor(.white)
-                            .frame(width: 89, height: 32)
-                            .background(Color("DarkGray"))
-                            .cornerRadius(10)
-                            .frame(maxWidth: .infinity, alignment: .trailing)
-                            .padding()
-                    }
-                    
-                    HStack {
-                        Circle()
-                            .frame(width: 20, height: 20)
-                            .foregroundColor(Color("DarkGray"))
-                        Circle()
-                            .frame(width: 20, height: 20)
-                            .foregroundColor(Color("Gray"))
-                        Circle()
-                            .frame(width: 20, height: 20)
-                            .foregroundColor(Color("Gray"))
-                    }
+                    emailTextField
+                    checkEmailButton(email: email)
+                    dots
                 }
             }
             .frame(width: 327, height: 275)
             .background(Color("Background"))
             .cornerRadius(10)
+        }
+    }
+    func checkEmailButton(email: String)-> some View{
+        Button {
+            viewmodel.sendEmail(email: email)
+        } label: {
+            Text("Continuar")
+                .foregroundColor(.white)
+                .frame(width: 89, height: 32)
+                .background(Color("DarkGray"))
+                .cornerRadius(10)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding()
+        }.background(
+            NavigationLink(destination: RecoverPasswordCodeView(), isActive: $viewmodel.shouldShowCode) {
+                EmptyView()
+            }
+        )
+    }
+    var emailTextField: some View {
+        VStack(spacing:2){
+            Text("Email")
+                .frame(maxWidth: 234, alignment: .leading)
+            TextField("",text: $email)
+                .padding().frame(width: 234, height: 27)
+                .background(Color(.white))
+                .cornerRadius(10)
+        }
+    }
+    var logo: some View{
+        Image("Logo")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 125, height: 80)
+    }
+    var dots: some View{
+        HStack {
+            Circle()
+                .frame(width: 20, height: 20)
+                .foregroundColor(Color("DarkGray"))
+            Circle()
+                .frame(width: 20, height: 20)
+                .foregroundColor(Color("Gray"))
+            Circle()
+                .frame(width: 20, height: 20)
+                .foregroundColor(Color("Gray"))
         }
     }
 }
--- BetterPlay/Presentation/RecoverPassword/ViewModel/RecoverPassViewModel.swift
+++ BetterPlay/Presentation/RecoverPassword/ViewModel/RecoverPassViewModel.swift
@@ -6,3 +6,102 @@
 //
 
 import Foundation
+extension RecoverPassEmailView{
+    class ViewModel:ObservableObject{
+        @Published var shouldShowCode: Bool = false
+        @Published var shouldShowAlert: Bool = false
+        func sendEmail(email: String){
+            let url = "https://betterplay-backend-production.up.railway.app/api/users/sendEmail"
+            let params: [String: String] = ["email": email]
+            
+            NetworkHelper.shared.requestProvider(url: url, type: .POST, params: params) { data, response, error in
+                if let error = error {
+                    self.onError(error: [error.localizedDescription])
+                } else if let data = data, let response = response as? HTTPURLResponse{
+                    print(response.statusCode)
+                    self.onSuccess(data: data, response: response)
+                }
+            }
+        }
+        func onSuccess(data: Data, response: HTTPURLResponse) {
+            do{
+                if response.statusCode != 200 {
+                }else{
+                    shouldShowCode = true
+                }
+            } catch {
+                self.onError(error: [error.localizedDescription])
+            }
+        }
+        func onError(error: [String]) {
+            shouldShowAlert = true
+            print(error)
+        }
+    }
+}
+extension RecoverPasswordCodeView{
+    class ViewModel:ObservableObject{
+        @Published var shouldShowNewPass: Bool = false
+        @Published var shouldShowAlert: Bool = false
+        func checkCorrectSecretCode(code: String){
+            let url = "https://betterplay-backend-production.up.railway.app/api/users/checkCorrectSecretCode"
+            let params: [String: String] = ["code": code]
+            
+            NetworkHelper.shared.requestProvider(url: url, type: .POST, params: params) { data, response, error in
+                if let error = error {
+                    self.onError(error: [error.localizedDescription])
+                } else if let data = data, let response = response as? HTTPURLResponse{
+                    print(response.statusCode)
+                    self.onSuccess(data: data, response: response)
+                }
+            }
+        }
+        func onSuccess(data: Data, response: HTTPURLResponse) {
+            do{
+                if response.statusCode != 200 {
+                }else{
+                    shouldShowNewPass = true
+                }
+            } catch {
+                self.onError(error: [error.localizedDescription])
+            }
+        }
+        func onError(error: [String]) {
+            shouldShowAlert = true
+            print(error)
+        }
+    }
+}
+extension NewPasswordView{
+    class ViewModel:ObservableObject{
+        @Published var shouldShowLogin: Bool = false
+        @Published var shouldShowAlert: Bool = false
+        func changePassword(password: String){
+            let url = "https://betterplay-backend-production.up.railway.app/api/users/changePassword"
+            let params: [String: String] = ["password": password]
+            
+            NetworkHelper.shared.requestProvider(url: url, type: .POST, params: params) { data, response, error in
+                if let error = error {
+                    self.onError(error: [error.localizedDescription])
+                } else if let data = data, let response = response as? HTTPURLResponse{
+                    print(response.statusCode)
+                    self.onSuccess(data: data, response: response)
+                }
+            }
+        }
+        func onSuccess(data: Data, response: HTTPURLResponse) {
+            do{
+                if response.statusCode != 200 {
+                }else{
+                    shouldShowLogin = true
+                }
+            } catch {
+                self.onError(error: [error.localizedDescription])
+            }
+        }
+        func onError(error: [String]) {
+            shouldShowAlert = true
+            print(error)
+        }
+    }
+}
